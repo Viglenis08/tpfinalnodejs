53a435a697f67593c8ed00a98379d7b4787804a6
Viglenis08 <viglenis1986@gmail.com>
2025-07-28 12:49:34 -0300

    verce4l

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import serverless from 'serverless-http';
-
+import app from '../index.js';
 import productsRouter from '../src/routes/products.router.js';
 import authRouter from '../src/routes/auth.router.js';
 import { authentication } from '../src/middlewares/auth.middleware.js';
